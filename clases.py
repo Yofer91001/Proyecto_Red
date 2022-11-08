9b6981a83097191cb040215c27a376852dc8392f
Yofer91001 <61855836+Yofer91001@users.noreply.github.com>
2022-11-08 17:42:04 -0500

    Inicializacion clases nodo y arista

--- clases.py
+++ clases.py
@@ -1,15 +1,24 @@
 
- class Router:
+class Router:
   __init__(self):
     self.ip = ip
     self.ip_table = list()
    
   
-  class Paquete:
+class Paquete:
     def __init__(self, load):
       self.load = load
       self.size = 0
       for _ in range(len(load)):
         self.size += 1
+        
+        
+class Node:
+    def __init__():
+     pass
+    
+class Edge_
+ def __init__():
+  pass
         
  
